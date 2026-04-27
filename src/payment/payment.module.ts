c8943d6a90a33f60cbb4ad12a37de316ce812cc2
Nodirbek1104 <iskandarovnodirbek27@gmail.com>
2026-04-27 22:04:58 +0500

    fix: eskiz error logging

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -3,9 +3,13 @@ import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Card } from './entities/payment.entity';
+import { ConfigModule } from '@nestjs/config'; // 1. Shuni import qiling
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card])],
+  imports: [
+    TypeOrmModule.forFeature([Card]),
+    ConfigModule, // 2. Imports qismiga qo'shing
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService]
